de32815bb548a4d5906964c52df19b34a7ae6528
James Doran <jysdoran@gmail.com>
2016-07-13 16:26:48 +0800

    Change build type, add "joystick-ish" functionality to a "weapon"
    Also change scale on Player object

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -8,7 +8,9 @@ public class playerController : MonoBehaviour {
     public bool devMouseControl = false;
     public Vector3 mousePosOld;
     public bool inAir;
-	
+
+    public GameObject weapon;
+
 	// Update is called once per frame
 	void Update () {
 
@@ -21,6 +23,7 @@ public class playerController : MonoBehaviour {
             if (Input.touchCount > 0)
             {
                 mostRecentTouch = Input.touches[0];
+                weapon.SetActive(true);
                 if (mostRecentTouch.phase == TouchPhase.Began)
                 {
                     initalTouchPosition = Camera.main.ScreenToWorldPoint(mostRecentTouch.position);
@@ -31,7 +34,22 @@ public class playerController : MonoBehaviour {
                     {
                         AttemptJump();
                     }
+                    Vector2 totalDelta = Camera.main.ScreenToWorldPoint(mostRecentTouch.position);
+                    totalDelta -= initalTouchPosition; //On a seperate line because apparently previous output must be type cast first
+                    if (totalDelta.x >= 0)//(mostRecentTouch.deltaPosition.x >= 0)
+                    {
+                        //weapon.transform.localEulerAngles = new Vector3(0f, 0f, Mathf.Atan(mostRecentTouch.deltaPosition.y / mostRecentTouch.deltaPosition.x) * 180 / Mathf.PI);
+                        weapon.transform.localEulerAngles = new Vector3(0f, 0f, Mathf.Atan(totalDelta.y / totalDelta.x) * 180 / Mathf.PI);
+                    }
+                    else
+                    {
+                        //weapon.transform.localEulerAngles = new Vector3(0f, 0f, 180 + Mathf.Atan(mostRecentTouch.deltaPosition.y / mostRecentTouch.deltaPosition.x) * 180 / Mathf.PI);
+                        weapon.transform.localEulerAngles = new Vector3(0f, 0f, 180 + Mathf.Atan(totalDelta.y / totalDelta.x) * 180 / Mathf.PI);
+                    }
                 }
+            } else
+            {
+                weapon.SetActive(false);
             }
         } else
         {
